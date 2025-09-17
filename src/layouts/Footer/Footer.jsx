46c60d02aaa8187f0322cec603bee56bfd0b0c6d
Alexis Bouvry <bouvryalexis@gmail.com>
2025-09-17 12:34:52 +0200

    add footer and remove old header

---
+++ src/layouts/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.scss'
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__disclaimer">© 2025 Callouu</p>
+    </footer>
+  )
+}
+
+export default Footer
