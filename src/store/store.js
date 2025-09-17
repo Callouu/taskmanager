3829e0b3c53c8ca798d3e50624aa697707fd61e8
Alexis Bouvry <bouvryalexis@gmail.com>
2025-09-17 12:46:51 +0200

    add comment

--- src/store/store.js
+++ src/store/store.js
@@ -3,13 +3,14 @@ import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, 
 import storage from 'redux-persist/lib/storage';
 import rootReducer from './reducers';
 
+// Configuration for redux-persist
+// Storage in the localStorage of the browser
 const persistConfig = {
   key: 'root',
   storage,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
