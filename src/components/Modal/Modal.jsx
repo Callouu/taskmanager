af1e73de01598309a8cfb7052800f3400317258f
Alexis Bouvry <bouvryalexis@gmail.com>
2025-09-17 12:34:16 +0200

    add modal component

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Modal.scss";
+
+function Modal({ open, onClose, children }) {
+  if (!open) return null;
+  return (
+    <div className="modal-backdrop" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
